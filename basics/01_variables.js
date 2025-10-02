dc22f625e306d33722069d407b35513e8903fe59
Anshika Shivhare <anshika494shiv@gmail.com>
2025-10-02 09:37:44 +0000

    learned variables and dtypes

---
+++ basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=122314 //cant be changed
+let accountEmail="abc@gmail.com"
+var passw="12345"
+accountCity="jaipur"
+let accountState;
+
+passw="12121"
+console.table([accountId,accountEmail,passw,accountCity,accountState])
+// var not used bcz of scope problem
