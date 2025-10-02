d31f44f8bd9ca1f0a2f0305a4d0aa25e454ea0e8
codeitanshika <anshika494shiv@gmail.com>
2025-10-02 15:42:15 +0530

    learned datatypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) //error bcz in nodejs, not in dev console or browser
+//js engine is in browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value, representation of empty value 
+// undefined =>  when value not defined or assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
