ac8641d16db1a16544d9de72c713d1df4851eff6
Anshika Shivhare <anshika494shiv@gmail.com>
2026-06-06 15:54:01 +0000

    basics done

---
+++ basics/07_date_and_time.js
@@ -0,0 +1,18 @@
+  let d=new Date()
+  console.log(d)
+  console.log(d.toLocaleTimeString())
+    console.log(d.toDateString())
+      console.log(d.toString())
+        console.log(d.toTimeString())
+        console.log(typeof d)
+/**
+* 11:34:23 AM
+Thu Jun 04 2026
+Thu Jun 04 2026 11:34:23 GMT+0000 (Coordinated Universal Time)
+11:34:23 GMT+0000 (Coordinated Universal Time)
+object */
+
+let mycreatedate=new Date(2023,0,23,12,30)//Mon Jan 23 2023 12:30:00 GMT+0530 (India Standard Time)
+let mytimestamp=Date.now()//value in millisec
+
+//
