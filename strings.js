1c3877f554aed14f75289903e9dfec071715acb0
Anshika Shivhare <anshika494shiv@gmail.com>
2026-06-27 17:48:55 +0000

    strings in js

---
+++ strings.js
@@ -0,0 +1,9 @@
+let str="JavaScript";
+
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.slice(0,4));
+console.log(str.includes("Script"));
+console.log(str.replace("Java","Type"));
+console.log(str.split(""));
