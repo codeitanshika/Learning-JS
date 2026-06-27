7cd26cbe595e00540e3034510a03f85b0db58601
Anshika Shivhare <anshika494shiv@gmail.com>
2026-06-27 17:47:37 +0000

    loops in js

---
+++ loops.js
@@ -0,0 +1,42 @@
+// For Loop
+
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+// While Loop
+
+let i=1;
+
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+// Do While
+
+let j=1;
+
+do{
+    console.log(j);
+    j++;
+}while(j<=5);
+
+// For Of
+
+let arr=[10,20,30];
+
+for(let value of arr){
+    console.log(value);
+}
+
+// For In
+
+let obj={
+    name:"Anshika",
+    age:21
+};
+
+for(let key in obj){
+    console.log(key,obj[key]);
+}
