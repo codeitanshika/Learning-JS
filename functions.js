d6d58a52d9934cd05f59845059f4a8a80bb209fd
Anshika Shivhare <anshika494shiv@gmail.com>
2026-06-27 17:48:23 +0000

    functions in js

---
+++ functions.js
@@ -0,0 +1,21 @@
+// Function Declaration
+
+function greet(name){
+    return "Hello " + name;
+}
+
+console.log(greet("Anshika"));
+
+// Function Expression
+
+const square = function(num){
+    return num*num;
+};
+
+console.log(square(5));
+
+// Arrow Function
+
+const add=(a,b)=>a+b;
+
+console.log(add(10,20));
