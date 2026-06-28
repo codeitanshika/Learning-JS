a422e7d680d4497e0960e962dd1f6380e339a586
Anshika Shivhare <anshika494shiv@gmail.com>
2026-06-28 17:21:47 +0000

    learned about scopes

---
+++ scope.js
@@ -0,0 +1,23 @@
+let a=10;
+
+function test(){
+
+    let b=20;
+
+    if(true){
+        let c=30;
+        console.log(c);
+    }
+
+    console.log(b);
+}
+
+test();
+
+console.log(a);
+
+// Hoisting
+
+console.log(x);
+
+var x=50;
