a54e7e98a62f3fa3c719266dcd91f069eebc83de
Anshika Shivhare <anshika494shiv@gmail.com>
2026-06-28 17:23:18 +0000

    promises

---
+++ promises.js
@@ -0,0 +1,14 @@
+let promise=new Promise((resolve,reject)=>{
+
+    let success=true;
+
+    if(success)
+        resolve("Success");
+    else
+        reject("Failed");
+
+});
+
+promise
+.then(result=>console.log(result))
+.catch(error=>console.log(error));
