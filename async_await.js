3048534cae3d8b1724748d7949630366d64108ed
Anshika Shivhare <anshika494shiv@gmail.com>
2026-06-28 17:24:00 +0000

    async await in js

---
+++ async_await.js
@@ -0,0 +1,23 @@
+function fetchData(){
+
+    return new Promise(resolve=>{
+
+        setTimeout(()=>{
+
+            resolve("Data Received");
+
+        },2000);
+
+    });
+
+}
+
+async function getData(){
+
+    let result=await fetchData();
+
+    console.log(result);
+
+}
+
+getData();
