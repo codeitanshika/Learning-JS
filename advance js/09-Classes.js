43d6b15ceba105f10be65e7926c85278258f120c
Anshika Shivhare <anshika494shiv@gmail.com>
2026-07-14 17:59:19 +0000

    classes in js

---
+++ advance js/09-Classes.js
@@ -0,0 +1,14 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    introduce() {
+        console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+}
+
+const p1 = new Person("Anshika", 21);
+
+p1.introduce();
