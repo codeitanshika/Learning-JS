582a5648b68a50501d93549c310518b0b9e27fb6
Anshika Shivhare <anshika494shiv@gmail.com>
2026-07-16 17:53:49 +0000

    post fetch request

--- advance js/25-FetchAPI.js
+++ advance js/25-FetchAPI.js
@@ -10,3 +10,25 @@ async function getUsers() {
 }
 
 getUsers();
+
+async function createUser() {
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: "John",
+                age: 25
+            })
+        }
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+}
+
+createUser();
