8c1a631061524a8b5de6ccf947e751c7d53a36df
Anshika Shivhare <anshika494shiv@gmail.com>
2026-07-16 17:53:31 +0000

    get-fetch reques

---
+++ advance js/25-FetchAPI.js
@@ -0,0 +1,12 @@
+// get
+async function getUsers() {
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+    );
+
+    const users = await response.json();
+
+    console.log(users);
+}
+
+getUsers();
