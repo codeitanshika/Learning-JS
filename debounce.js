f125d219870ac7b30be8dcc0b2182697530cc0aa
Anshika Shivhare <anshika494shiv@gmail.com>
2026-07-21 17:31:18 +0000

    implemented debounce

---
+++ debounce.js
@@ -0,0 +1,15 @@
+// debounce.js
+
+function debounce(func, delay) {
+    let timerId;
+
+    return function (...args) {
+        const context = this;
+
+        clearTimeout(timerId);
+
+        timerId = setTimeout(() => {
+            func.apply(context, args);
+        }, delay);
+    };
+}
