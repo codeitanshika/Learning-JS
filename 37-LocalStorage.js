3125543395d163f89c4bb19a2ee62d7a58bce29b
Anshika Shivhare <anshika494shiv@gmail.com>
2026-07-24 18:01:45 +0000

    local storage example

--- 37-LocalStorage.js
+++ 37-LocalStorage.js
@@ -11,3 +11,16 @@ localStorage.removeItem("username");
 
 // Delete everything
 localStorage.clear();
+
+const user = {
+    name: "John",
+    age: 25
+};
+
+localStorage.setItem("user", JSON.stringify(user));
+
+const storedUser = JSON.parse(
+    localStorage.getItem("user")
+);
+
+console.log(storedUser.name);
